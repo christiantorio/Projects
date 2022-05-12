8addb354fa39cfaed280de5cf24c48ce4b47aca9
Christian Torio <christiantorio@aol.com>
2022-05-12 15:29:37 -0700

    adding new function to search for ID and assert

--- python/automation/webtest_nike.py
+++ python/automation/webtest_nike.py
@@ -2,20 +2,29 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonOrgSearch(unittest.TestCase):
+class WebSearch(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path="chromedriver")
 
     def test_search(self):
+        print("Testing search feature...")
         driver = self.driver
         driver.get("http://www.nike.org")
         self.assertIn("Nike", driver.title)
         elem = driver.find_element_by_id("VisualSearchInput")
         elem.send_keys("Air Force 1")
         elem.send_keys(Keys.RETURN)
-        self.assertIn("Nike", driver.title)
-
+        driver.back()
+    
+    def test_element(self):
+        print("Testing how to find elements")
+        driver = self.driver
+        driver.get("http://www.nike.org")
+        elem = driver.find_element_by_id("hf_title_sigin_membershipt")
+        self.assertIn("Sign In", driver.id)
+        
+        
     def tearDown(self):
         self.driver.close()
 
